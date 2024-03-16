4224c6fbc2bfb38956e1ddfb9ecf121217407f80
thethiago27 <thethiago27@gmail.com>
2024-03-16 15:54:57 -0400

    init: change pipeline

--- src/env/config.js
+++ src/env/config.js
@@ -3,5 +3,5 @@ require('dotenv').config()
 module.exports = {
   TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
   NODE_ENV: process.env.NODE_ENV,
-  USER_RECIPIENT_ID: process.env.USER_RECIPIENT
+  USER_RECIPIENT_ID: process.env.USER_RECIPIENT_ID
 }
